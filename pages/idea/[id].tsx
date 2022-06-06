0d16039ba526e5ca16a05786c35399943e57201f
Serenidad <95105859+SerenityUX@users.noreply.github.com>
2022-06-06 16:55:55 -0400

    connected to database

---
+++ pages/idea/[id].tsx
@@ -0,0 +1,57 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+import Grey from '../../assets/serenidad_grey.png'
+
+
+export async function getServerSideProps(context: any) {
+  const { id } = context.params;
+
+  const ideas = await fetch(
+    "https://dev.createforever.media/api:l36tuZkn/ideas"
+  ).then((res) => res.json());
+
+  const idea = await fetch(
+    "https://dev.createforever.media/api:l36tuZkn/ideas/" + id
+  ).then((res) => res.json());
+
+  return { props: { idea, ideas } };
+}
+export default function Home(props: any) {
+  console.log(props.ideas)
+  console.log(props.idea.idea_image)
+
+  return (
+    <div>
+      <div className={styles.side_bar}>
+        <h1 className={styles.main_title}>Serenidad</h1>
+        {props.ideas.map((idea: any, indexposition: any) => {
+          return (
+          <div>
+            <a href={"./idea/" + idea.id} key={indexposition}>{idea.title}</a>
+          </div>
+        )})}
+        </div>
+      <div className={styles.main_content}>
+            <Image width={496} height={216} src={props.idea.idea_image.url}></Image>
+            <p>
+                {props.idea.title}
+            </p>
+            <p>
+              {props.idea.description} 
+            </p>
+
+            {props.idea.question_and_answer.map((question: any, indexpositiontwo: any) => {
+          return (
+          <div>
+            <p>{question.question}</p>
+            <p>{question.answer}</p>
+          </div>
+        )})}
+
+      </div>
+
+    </div>
+  )
+}
