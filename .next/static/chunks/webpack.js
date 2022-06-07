c77f7af8a806651d13748ab4adcc7ced31e9a718
Serenidad <95105859+SerenityUX@users.noreply.github.com>
2022-06-07 09:26:09 -0400

    font change

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d0d55a98b7f2143a"; }
+/******/ 		__webpack_require__.h = function() { return "294c569b33844733"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.88c5299582343def.hot-update.js
+++ .next/static/webpack/webpack.88c5299582343def.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "93de44f6c1315178"; }
+/******/ 	__webpack_require__.h = function() { return "7f511b2ac6e347a6"; }
 /******/ }();
 /******/ 
 /******/ }
